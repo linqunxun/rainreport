4b88fa7be484660702191abdcfa0985cb875d306
linqunxun@wisight.cn <linqunxun@1026>
2022-01-18 10:58:02 +0800

    init

--- rainreport.py
+++ rainreport.py
@@ -3,15 +3,14 @@
 # 调用方式一：获取当天的数据：$ python rainreport.py
 # 调用方式二：获取一段时期的数据：$ python rainreport.py "2016-09-01 00:00" "2016-09-04 00:00"
 
-
-import urllib2
+import time
+import urllib.request
 import re
-import HTMLParser
 import sys
 import datetime
 import urllib  
-import cookielib
-#print html
+
+# ################################ 定义开始 ###############################
 
 MATCH_NO = 0
 MATCH_TR_BEGIN = 1
@@ -19,111 +18,100 @@ MATCH_TR_END = 2
 MATCH_TD_BEGIN = 3
 MATCH_TD_END = 4
 MATCH_TABLE_END = 5
+DATE_FORMAT = "%Y-%m-%d"
 
-global_match_state = MATCH_NO
+def grab(date, file):
+	print("正在采集%s的数据..." %(date.strftime(DATE_FORMAT)))
+	global_match_state = MATCH_NO
+	write_line = ""
 
-begin_time = ""
-end_time = ""
+	txt_time1 = date.strftime("%Y-%m-%d 00:00")
+	txt_time2 = (date + datetime.timedelta(days = 1)).strftime("%Y-%m-%d 00:00")
 
-print( sys.argv )
-if 1 == len(sys.argv) : 
-	write_file_bl = open("./" + now.strftime('%m-%d') + "_big_lit.cvs", 'a')
-	write_file_area = open("./" + now.strftime('%m-%d') + "_area.cvs", 'a')
-else:
-    begin_time = sys.argv[1]
-    end_time = sys.argv[2]
-    filename = begin_time + "_" + end_time
-    filename = filename.replace(":", "")
-    filename = filename.replace("-", "")
-    filename = filename.replace(" ", "")
-    write_file_bl = open("./" + filename + "_big_lit.cvs", 'a')
-    write_file_area = open("./" + filename + "_area.cvs", 'a')	
-
-
-################################ 代码开始 ###############################
-now = datetime.datetime.now()
-
-
-write_file = write_file_bl
-write_line = ""
-
-url = 'http://www.gdwater.gov.cn:9001/Report/RainReport.aspx'
-if 0 == len(begin_time):
-    response = urllib2.urlopen(url)
-else:
-    viewstate_file = open("VIEWSTATE", "r")
-    view_state = viewstate_file.read().strip('\n')
-    viewstate_file.close()
-
-    req = urllib2.Request(url)
-    data = urllib.urlencode({
-        '__VIEWSTATE':view_state,
-		'__EVENTVALIDATION':'/wEWHALKnaiwDgKfts6aDQLzpIrbDwLzpP78BALzpNKRDALzpMaqBQLzpLrOAgLzpK7jCwLzpIKEAwLzpLbvDQLzpKqABQKYyoPhDgKYyve6BgKYyuvfDwKYyt/wBAKYyrOUDAKYyqepBQKYytuRBgKYys+qDwK9093dAgLslpexCALslovKAQLslv/vDgKzwPf7DQKO5oKYAQKN5oKYAQLjwOP9CALcoYgbAFxfEg5XjmrAgNSS7S3F/N2OWNY=',
-        'txt_time1':begin_time, 
-        'txt_time2':end_time, 
-        'btn_query':'查询'
-        #'ddl_addvcd':'全部'
+	req = urllib.request.Request(url)
+	data = urllib.parse.urlencode({
+		'__VIEWSTATE':view_state,
+		'__EVENTVALIDATION':eventvalidation,
+		'txt_time1':txt_time1, 
+		'txt_time2':txt_time2, 
+		'btn_query':'查询'
+		#'ddl_addvcd':'全部'
 		})
+	#enable cookie  
+	opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor())  
+	response = opener.open(req, bytes(data, encoding='utf-8')) 
 
-    print(data)
-    #enable cookie  
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())  
-    response = opener.open(req, data) 
+	line_list = response.readlines()
+	for lineBytes in line_list:		#遍历网页的每一行  
+		line = str(lineBytes, encoding='utf-8').strip()		#去除行首位的空格，习惯性写法
+		if line == '':
+			continue
 
+		if MATCH_NO == global_match_state:
+			if re.match( '<tr align="center" bgcolor=".', line ):
+				# print ('set state = MATCH_TR_BEGIN line = ' + line)
+				global_match_state = MATCH_TR_BEGIN
 
+		elif MATCH_TR_BEGIN == global_match_state:
+			write_line = date.strftime(DATE_FORMAT) + ","
+			if re.match('<td  class=tdleft>', line):
+				# print ('set state = MATCH_TD line = ' + line)
+				global_match_state = MATCH_TD_BEGIN
 
-html = response.read()
+		elif MATCH_TD_BEGIN == global_match_state:
+			if re.match('</td>', line) or '' == line:
+				global_match_state = MATCH_TD_END
+			else:
+				write_line = write_line + line + ','
 
-#type = sys.getfilesystemencoding()
-#html = html.decode("UTF-8").encode(type)
+		elif MATCH_TD_END == global_match_state:
+			if re.match('<td +class=.', line):
+				# print ('set state = MATCH_TD line = ' + line)
+				global_match_state = MATCH_TD_BEGIN
+			elif re.match('</tr>', line):
+				# print ('write write_line to file, write_line = ' + write_line)
+				file.write( write_line + '\n' )
+			elif re.match('<tr align="center" bgcolor=".', line):
+				# print ('set state = MATCH_TR_BEGIN line = ' + line)
+				global_match_state = MATCH_TR_BEGIN
+			elif re.match('</table>', line):
+				global_match_state = MATCH_TABLE_END
 
-line_list = html.split('\r\n')
-html_parser = HTMLParser.HTMLParser()
+	print("采集%s的数据完成.\n" %(date.strftime(DATE_FORMAT)))
 
-for eachLine in line_list:		#遍历网页的每一行  
-	line = eachLine.strip()		#去除行首位的空格，习惯性写法
-	if line == '':
-		continue
+# ################################ 逻辑处理开始 ###############################
 
-	#line = line.decode("UTF-8")
+try :
+	if 1 == len(sys.argv) : 
+		begin_date = datetime.datetime.now()
+		end_date = begin_date
+		filename = begin_date.strftime(DATE_FORMAT)
+	elif 2 == len(sys.argv) :
+		begin_date = datetime.datetime.strptime(sys.argv[1], DATE_FORMAT)
+		end_date = begin_date
+		filename = begin_date.strftime(DATE_FORMAT)
+	elif 3 == len(sys.argv) :
+		begin_date = datetime.datetime.strptime(sys.argv[1], DATE_FORMAT)
+		end_date = datetime.datetime.strptime(sys.argv[2], DATE_FORMAT)
+		filename = begin_date.strftime(DATE_FORMAT) + "_" + end_date.strftime(DATE_FORMAT)
+	else :
+		raise Exception("参数个数有误")
+except Exception as err :
+	print("异常信息：", err)
+	print("参数解析有误！\n● 不传参则取当天数据\n● 传一个日期则取该日期数据\n● 传两个日期则取日期范围数据\n● 日期格式为：2022-01-01")
+	exit()
 
-	if MATCH_NO == global_match_state:
-		if re.match( '<tr align="center" bgcolor=".', line ):
-			print 'set state = MATCH_TR_BEGIN line = ' + line
-			global_match_state = MATCH_TR_BEGIN
-			write_file.write( "市,市(县),站点,累计雨量(毫米),\n" )
+print("准备开始采集，日期为：%s" %(filename))
 
-	elif MATCH_TR_BEGIN == global_match_state:
-		write_line = ""
-		if re.match('<td  class=tdleft>', line):
-			print 'set state = MATCH_TD line = ' + line
-			global_match_state = MATCH_TD_BEGIN
+file = write_file_bl = open("./" + filename + "_big_lit.cvs", 'a')
+url = 'http://210.76.80.76:9001/Report/RainReport.aspx'
+viewstate_file = open("VIEWSTATE", "r")
+view_state = viewstate_file.read().strip('\n')
+viewstate_file.close()
+eventvalidation = '/wEdABx9RZMmw1LG+yU0BQd3TV2rD5R4QB07L6EPE8hbiA1HCjQe693E4rfh8w4KrFy5Bj3qc95Ub9AmjsCaEcQ1PRGXJY9KOVS66bYw3q07YOQVt4DmtqNRf8Rh8CBmDXE37HiN5RVSvKkNx6BevOTmSZwzKK4M9qnFNMlL/90YA0v2KsxbrBB3jo9Cugoavc63zLm9My41G4k2bD1H9/DevJa7wcjQz304htdplq4o6qLjU7BToL28oNzz/tjDoVVVIhiD3oM18XDf8vos8kRdlCgWpjFPpuO0nCnf++I958L2pfPnIB5/wJLV8h40L35u+d4vFTOoUBfCvciBoq6O3SZ4CMvy4Ne/abH/S8UnCo91U4bK+iIVPQ9Qmq1oIGHjR+rSlof14T3WkNe3Bfqmtxf9LKiIdtM0Qyv+SkqglRIe2eX+ybSo2jt25MGjS+iu0HzzRgyGF4e9D60kZeFjGcVG3Mp8k0MqtIykrMCeFt1vrvYBoMfVKie2hgAVD0f/Dy7Ev58OiHZScWJMXWzdRGgTupO9qKKX2H5OVAecnhdKrp/niFT6iq+w7w6u1SnFjkNYEmXUkIdwCjm7RrNxm+3ecjUCzmBKr0HCW4ODG/DfLtR6irz7pCQihoGrG/aDEXc='
 
-	elif MATCH_TD_BEGIN == global_match_state:
-		if re.match('</td>', line) or '' == line:
-			global_match_state = MATCH_TD_END
-		else:
-			write_line = write_line + line + ','
-
-	elif MATCH_TD_END == global_match_state:
-		if re.match('<td +class=.', line):
-			print 'set state = MATCH_TD line = ' + line
-			global_match_state = MATCH_TD_BEGIN
-		elif re.match('</tr>', line):
-			print 'write write_line to file, write_line = ' + write_line
-			write_file.write( write_line + '\n' )
-		elif re.match('<tr align="center" bgcolor=".', line):
-			print 'set state = MATCH_TR_BEGIN line = ' + line
-			global_match_state = MATCH_TR_BEGIN
-		elif re.match('</table>', line):
-			global_match_state = MATCH_TABLE_END
-			write_file.close()
-
-	elif MATCH_TABLE_END == global_match_state:
-		if re.match( '<tr align="center" bgcolor=".', line ):
-			print 'set state = MATCH_TR_BEGIN line = ' + line
-			global_match_state = MATCH_TR_BEGIN
-			write_file = write_file_area
-			write_file.write( "市,市(县),站点,累计雨量(毫米),\n" )
-
-
+file.write( "日期,市,市(县),站点,累计雨量(毫米),\n" )
+for i in range((end_date - begin_date).days + 1):
+	grab(begin_date + datetime.timedelta(days=i), file)
+file.close()
+print("采集完毕，生成的文件名为：%s_big_lit.cvs" %(filename))
---
+++ readme.md
@@ -0,0 +1,11 @@
+# 雨情报表采集程序
+> 该程序采集来自[雨情报表](http://210.76.80.76:9001/Report/RainReport.aspx)的数据</br>
+> 程序使用python3进行开发
+
+## 使用方式
+```shell
+./rainreport.py [begin_date] [end_date]
+```
+
+## 更新日志
+- 2022-01-18 完善程序，支持日期段采集
